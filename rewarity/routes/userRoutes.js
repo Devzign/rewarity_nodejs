d1f914d902e70f86edebd4280a16fb966926e568
Amit Sinha <amitsinhadeveloper@gmail.com>
2025-11-09 10:07:44 +0530

    modify the position

--- rewarity/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { createUserType, createUser } = require('../controllers/userController');
-
-const router = express.Router();
-
-router.post('/create-type', createUserType);
-router.post('/create-user', createUser);
-
-module.exports = router;
